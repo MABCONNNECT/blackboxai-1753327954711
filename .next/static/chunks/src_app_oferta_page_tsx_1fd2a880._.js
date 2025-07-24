4a06ba323d1708269954ff74b492633c81ac9e4c
MABCONNNECT <mabconnect10@gmail.com>
2025-07-23 22:32:50 -0500

    Add .next/static/chunks/src_app_oferta_page_tsx_1fd2a880._.js 1753327973626

---
+++ .next/static/chunks/src_app_oferta_page_tsx_1fd2a880._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_app_oferta_page_tsx_25521a9b._.js"
+],
+    source: "dynamic"
+});
